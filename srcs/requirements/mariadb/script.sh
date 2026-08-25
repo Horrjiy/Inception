17a04f17c41f3e8d214212a13e35aff6635bb95b
Mischa Pöplow <mpoplow@student.42heilbronn.de>
2026-08-25 17:04:41 +0200

    Mariadb script & dockerfile update

---
+++ srcs/requirements/mariadb/script.sh
@@ -0,0 +1,24 @@
+mariadbd
+systemctl enable --now mariadb
+
+mariadb -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MDB_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS '${MDB_DATABASE}' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS '${MDB_USER}'@'%' IDENTIFIED BY '${MDB_PASSWORD}';
+GRANT ALL PRIVILEGES ON '${MDB_DATABASE}'.* TO '${MDB_USER}'@'%';
+FLUSH PRIVILEGES
+EOF
+
+## DEBUG
+
+# echo
+# echo "────────────────────────────────────────"
+# echo "  Root password : ${MDB_ROOT_PASSWORD}"
+# echo "  Database      : ${MDB_DATABASE}"
+# echo "  User          : ${MDB_USER}"
+# echo "  User password : ${MDB_PASSWORD}"
+# echo "────────────────────────────────────────"
+# echo
+
+systemctl restart mariadb
+exec mariadbd
