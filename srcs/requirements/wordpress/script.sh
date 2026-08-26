2ff7933e6d3df6ba452cc02628e289fa678b1a12
Mischa Pöplow <mpoplow@student.42heilbronn.de>
2026-08-26 15:09:10 +0200

    Main structure finished (bug hunting starting soon)

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env sh
+
+if [! wp core is-installed ]; then
+    chown -R www-data:www-data /var/www/html
+    wp core download --allow-root
+    wp config create --dbname=${MDB_DATABASE} --dbuser=${MDB_USER} --dbpass=${MDB_PASSWORD} --dbhost=${DBHOST}:3306 --allow-root
+
+    wp core install --url=mpoplow.42.fr --title="Schaschenk!" --admin_user=${WPAD_USER} --admin_password=${WPAD_PASSWORD} --admin_email=${WPAD_EMAIL} --allow-root
+    wp user create zweier zwei@drei.vier --user-pass=${ZWEIER_PASSWORD} --allow-root
+
+else
+    echo "wordpress has already been set up!"
+fi
+
+exec php-fpm8.2 -F
