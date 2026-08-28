a6cdeea423e17235b9b08eb0b50ea742be4827d5
mpoplow <mpoplow@student.42heilbronn.de>
2026-08-28 15:43:27 +0200

    Fixed some bugs and env problems

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env sh
 
-if [! wp core is-installed ]; then
+
+
+if  wp core is-installed --allow-root ; then  
+    echo "wordpress has already been set up!"
+else
     chown -R www-data:www-data /var/www/html
     wp core download --allow-root
     wp config create --dbname=${MDB_DATABASE} --dbuser=${MDB_USER} --dbpass=${MDB_PASSWORD} --dbhost=${DBHOST}:3306 --allow-root
 
     wp core install --url=mpoplow.42.fr --title="Schaschenk!" --admin_user=${WPAD_USER} --admin_password=${WPAD_PASSWORD} --admin_email=${WPAD_EMAIL} --allow-root
-    wp user create zweier zwei@drei.vier --user-pass=${ZWEIER_PASSWORD} --allow-root
-
-else
-    echo "wordpress has already been set up!"
+    wp user create zweier zwei@drei.vier --user_pass=${ZWEIER_PASSWORD} --allow-root
 fi
 
 exec php-fpm8.2 -F
